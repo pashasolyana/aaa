aec5f792368d7feae01e130eebbe4f8e044a7ced
ivan <ivan.safonov.09@mail.ru>
2023-05-25 23:07:54 +0300

    first screen adaptive

--- aaaa/client/src/Components/FirstScreen/FirstScreen.tsx
+++ aaaa/client/src/Components/FirstScreen/FirstScreen.tsx
@@ -1,8 +1,7 @@
-import { ChangeEvent, FC, useState } from 'react'
+import { FC } from 'react'
 import styles from './FirstScreen.module.scss'
-import { OtherService } from '../../../services/other/other.service'
-import getStatusGET from './getStatusGET'
 import { Link } from 'react-scroll'
+import Image from 'next/image'
 
 interface Tracker {
   vvv: () => void
@@ -18,61 +17,61 @@ export const FirstScreen: FC<Tracker> = ({
   number,
   setPath
 }) => {
+  const nav = () => {
+    setPath(1)
+    if (number.length > 0) vvv()
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.block}>
-        <div className={styles.blockLeft}>
-          <div className={styles.titleCont}>
-            <div className={styles.title}>Курьерская служба доставки</div>
-            <div className={styles.title}>Люблино Экспресс</div>
+    <div className={styles.cont}>
+      <div className={styles.info}>
+        <h1 className={styles.info__title}>
+          Курьерская служба доставки Люблино Экспресс
+        </h1>
+        <div className={styles.descCont}>
+          <div className={styles.descCont__arrow}>
+            <p>Доставим что угодно</p>
+            <Image src={'/arrow.svg'} width={31} height={42} alt='' />
+            <p>куда угодно. Наш сайт предоставляет</p>
           </div>
-          <div className={styles.subtitle}>Отправляй и получай!</div>
-          <div>
-            <Link
-              activeClass='active'
-              to='form'
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-              onClick={() => setPath(0)}
-              className={styles.lowHeaderText}
-            >
-              <button className={styles.btn}>Заказать</button>
-            </Link>
-          </div>
+          <p className={styles.descCont__text}>
+            возможность отслеживания статуса вашего заказа на каждом этапе
+            доставки, начиная от момента отправки до момента получения товара.
+          </p>
+          <p className={styles.descCont_mod}>
+            Доставим что угодно куда угодно. Наш сайт предоставляет возможность
+            отслеживания статуса вашего заказа на каждом этапе доставки, начиная
+            от момента отправки до момента получения товара.
+          </p>
         </div>
-        <div className={styles.blockRight}>
-          <div className={styles.track}>Номер заказа</div>
-          <div className={styles.trackInput}>
+      </div>
+      <div className={styles.traceCont}>
+        <h1 className={styles.traceCont__title}>Номер заказа</h1>
+        <div className={styles.traceCont__search}>
+          <div className={styles.traceCont__input}>
+            <Image src={'/search.svg'} width={15} height={15} alt='search' />
             <input
               type='text'
               value={number}
-              placeholder='Введите номер заказа'
+              placeholder='Номер заказа'
               onChange={handleChange}
             />
           </div>
-          <div>
-            <Link
-              activeClass='active'
-              to='track'
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-              onClick={() => setPath(1)}
-              className={styles.lowHeaderText}
-            >
-              <button
-                className={styles.btn}
-                onClick={() => (number.length > 0 ? vvv() : null)}
-              >
-                Отследить
-              </button>
-            </Link>
-          </div>
+          <Link
+            activeClass='active'
+            to='track'
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            onClick={nav}
+          >
+            <button>Отследить</button>
+          </Link>
         </div>
       </div>
+      <div className={styles.leftBorder}></div>
+      <div className={styles.rigthBorder}></div>
     </div>
   )
 }
