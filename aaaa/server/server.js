d294171c378a9d4bf59c42ea86c2c4eb13ce0116
pashasolyana <pavlolypko@mail.ru>
2023-05-27 22:02:44 +0300

    fix

--- aaaa/server/server.js
+++ aaaa/server/server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
-
+/*
 mongoose.connect("mongodb://127.0.0.1:27017/lublino").then(() => {
       try{
             console.log('mongodb connected')
@@ -14,7 +14,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/lublino").then(() => {
             console.log(`truble in connecting to db: ${e}`)
       }
 })
-
+*/
 app.listen(PORT, 'localhost', () => {
       console.log('server listening localhost', PORT);
 });
