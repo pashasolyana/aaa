1cd3203a4d6717b0f42374bb4e541ecbb26f33a0
ivan <ivan.safonov.09@mail.ru>
2023-05-28 14:47:49 +0300

    adaptive footer

--- aaaa/client/src/Components/Footer/Footer.tsx
+++ aaaa/client/src/Components/Footer/Footer.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import styles from './Footer.module.scss'
+import s from './Footer.module.scss'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className={styles.cont} id='documents'>
-      <div className={styles.wrapper}>
-        <h1 className={styles.title}>Документы</h1>
-        <p className={styles.desc}>Скоро здесь будет информация о нас!</p>
+    <div className={s.cont} id='documents'>
+      <div className={s.leftB}>
+        <span className={s.write}>Пишите нам</span>
+        <div className={s.ico}>
+          <Image src={'telegram.svg'} width={50} height={50} alt='telegram' />
+          <Image src={'/vk.svg'} width={50} height={50} alt='vk' />
+          <Image src={'/whatsApp.svg'} width={50} height={50} alt='whatsApp' />
+        </div>
+        <p>© ООО «Люблино экспресс», 2023</p>
+        <p>Политика обработки данных</p>
+      </div>
+      <div className={s.rightB}>
+        <span className={s.write}>Наши документы</span>
+        <p>Свидетельство</p>
+        <p>Лицензия</p>
+        <p>Документ номер один</p>
+        <p>Документ номер два</p>
       </div>
     </div>
   )
